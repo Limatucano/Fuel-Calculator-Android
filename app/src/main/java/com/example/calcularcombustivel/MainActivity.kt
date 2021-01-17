f050fc9fba16408dc98c00f055397ac9c7a3df1d
Matheus Lima <matheus.correa@mindtek.com.br>
2021-01-16 22:46:29 -0300

    ajustes e image na tela

--- app/src/main/java/com/example/calcularcombustivel/MainActivity.kt
+++ app/src/main/java/com/example/calcularcombustivel/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        supportActionBar?.hide()
         result= findViewById(R.id.resultado)
         resulttxt = findViewById(R.id.resultadotxt)
 
