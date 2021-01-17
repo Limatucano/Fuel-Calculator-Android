cc43092b4070c7a6359e6954658229018f7cbcb1
Matheus Lima <matheus.correa@mindtek.com.br>
2021-01-16 22:16:46 -0300

    beta

--- app/src/main/java/com/example/calcularcombustivel/MainActivity.kt
+++ app/src/main/java/com/example/calcularcombustivel/MainActivity.kt
@@ -1,19 +1,53 @@
 package com.example.calcularcombustivel
 
+import android.app.Activity
+import android.content.Context
+import android.icu.text.DecimalFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import javax.xml.transform.dom.DOMLocator
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity()
+{
+    private var litrosGasolina: Double = 0.0
+    private var gasto: Double = 0.0
+    private lateinit var result : TextView
+    private lateinit var resulttxt : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById(R.id.viewPrincipal)
 
-        fun calcular(view: View){
-            var temp: Double = editText.text.toString().toDouble()
+        result= findViewById(R.id.resultado)
+        resulttxt = findViewById(R.id.resultadotxt)
+
+        val button: Button = findViewById(R.id.btn)
+        var kmtotal: EditText = findViewById(R.id.kmTotal)
+        var consumoLitro: EditText = findViewById(R.id.consumoLitro)
+        var precoCombustivel: EditText = findViewById(R.id.precoCombustivel)
 
 
+        button.setOnClickListener {
+            it.hideKeyboard()
+           calcular(kmtotal.text.toString().toDouble(),consumoLitro.text.toString().toDouble(),precoCombustivel.text.toString().toDouble())
         }
+
+    }
+
+    fun calcular(kmtotal: Double, consumoLitro: Double, precoCombustivel: Double) {
+
+        litrosGasolina = kmtotal / consumoLitro
+        gasto = litrosGasolina * precoCombustivel
+        resulttxt.setText("Seu Resultado:")
+        result.setText("R$ "+gasto.toString())
+    }
+    fun View.hideKeyboard() {
+        val inputManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(windowToken, 0)
     }
 }
